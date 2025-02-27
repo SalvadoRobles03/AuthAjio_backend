a91a2fdef9a874b40e895bb62e7232f1882fd8aa
SalvadoRobles03 <salvador.a.robles@autozone.com>
2025-02-26 19:37:29 -0600

    📦 NEW: Initial Commit

---
+++ src/main/java/com/salvadorobles03/authajio/AuthAjioApplication.java
@@ -0,0 +1,13 @@
+package com.salvadorobles03.authajio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAjioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAjioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salvadorobles03/authajio/AuthAjioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvadorobles03.authajio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAjioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
